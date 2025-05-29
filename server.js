6a391ff7faf8800dd757f0c6c005c41df9622f5f
ToocaGroupLtda <toocagroup@gmail.com>
2025-05-29 18:14:08 -0300

    vamos

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const path = require('path');
+//simple express server to run frontend production build;
+const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(3000);
+
